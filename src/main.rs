7cec0c439f05b393df76649f2fe984160b0d623b
ranokhassanova <khasanova.rano@gmail.com>
2025-05-17 20:32:05 +0500

    switch player, added text to buttons

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ struct TicTacToeApp {
 #[derive(Debug, Clone)]
 enum Message {
     PlayerMove(Player),
-    // CellPressed(row, col),
+    CellPressed(usize, usize),
     XPlayerWon,
     OPlayerWon,
     Draw
@@ -30,25 +30,65 @@ enum Message {
 impl Sandbox for TicTacToeApp {
     type Message = Message;
     fn new() -> Self {
-        Self::default()
+        Self {
+            current_player: Some(Player::XPlayer),
+            ..Self::default()
+        }
     }
 
     fn title(&self) -> String {
         String::from("Tic — Tac — Toe")
     }
 
-    fn update(&mut self, _message: Self::Message) {
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::PlayerMove(player) => {
+                println!("Player x or o");
+            }
+            Message::CellPressed(row, col) => {
+                if self.game_field[row][col].is_none() {
+                    self.game_field[row][col] = Some(
+                        match self.current_player {
+                            Some(Player::XPlayer) => Cell::X,
+                            Some(Player::OPlayer) => Cell::O,
+                            None => Cell::X
+                        }
+                    );
 
+                    self.current_player = match self.current_player {
+                        Some(Player::XPlayer) => Some(Player::OPlayer),
+                        Some(Player::OPlayer) => Some(Player::XPlayer),
+                        None => Some(Player::XPlayer)
+                    }
+                }
+            }
+            Message::XPlayerWon => {
+                println!("Player x have won");
+            }
+            Message::OPlayerWon => {
+                println!("Player 0 have won");
+            }
+            Message::Draw => {
+                println!("Draw");
+            }
+        }
     }
 
     fn view(&self) -> Element<Message> {
 
         let col = Column::with_children(
-            self.game_field.iter().map(|row| {
-                Row::with_children(row.iter().map(|cell| {
-                    Button::new(Text::new(" "))
+            self.game_field.iter().enumerate().map(|(row_idx, row)| {
+                Row::with_children(row.iter().enumerate().map(|(col_idx, cell)| {
+                    let text = match cell {
+                        Some(Cell::X) => "X",
+                        Some(Cell::O) => "O",
+                        Some(Cell::Empty) => " ",
+                        None => " "
+                    };
+                    Button::new(Text::new(text))
                         .width(Length::Fill)
                         .padding(20)
+                        .on_press(Message::CellPressed(row_idx, col_idx))
                         .into()
                     })
                     .collect()
